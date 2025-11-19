b70d0a10c2f5cb705e5aa372e1668f76c5c5a50a
SAT-oO <z869liu@uwaterloo.ca>
2025-11-19 16:56:26 -0500

    test: added span re-enter

--- examples/simple_integration.rs
+++ examples/simple_integration.rs
@@ -18,14 +18,14 @@ async fn main() {
         .unwrap();
     let handle = tokio::spawn(task);
     let subscriber = tracing_subscriber::registry()
-        .with(tracing_subscriber::fmt::layer().with_filter(LevelFilter::ERROR))
+        .with(tracing_subscriber::fmt::layer().with_filter(LevelFilter::INFO))
         .with(layer);
 
     tracing::subscriber::set_global_default(subscriber).unwrap();
 
     let mut tasks = Vec::new();
 
-    for work_id in 0..4 {
+    for work_id in 0..1 {
         tasks.push(tokio::spawn(async move {
             let payload = "x".repeat(5);
             for i in 0..1 {
@@ -37,25 +37,39 @@ async fn main() {
                     big_msg = %payload,
                 );
 
-                let _gp_enter = gp_span.enter();
+                let gp_enter = gp_span.enter();
 
                 {
-                    let p_span =
-                        tracing::span!(Level::TRACE, "parent span", span_iteration = i, big_msg = %payload,);
-                    let _p_enter = p_span.enter();
+                    let p_span = tracing::span!(Level::TRACE, "parent span", big_msg = %payload,);
+                    let p_enter = p_span.enter();
 
-                    let msg: String = format!("random event No. {i}");
-
-                    tracing::event!(Level::DEBUG, message = msg, event_iteration = i, big_msg = %payload,);
-
-                    let c_span = tracing::span!(Level::INFO, "child span", span_iteration = i, big_msg = %payload);
-                    let _c_enter = c_span.enter();
                     {
-                        let msg: String = format!("random event No. {i} duplicate");
+                        let msg: String = format!("random event No. {i}");
 
-                        tracing::event!(Level::ERROR, message = msg, event_iteration = i, big_msg = %payload);
+                        tracing::event!(Level::DEBUG, message = msg, msg = %payload,);
+
+                        let c_span = tracing::span!(Level::INFO, "child span", span_iteration = i);
+                        let c_enter = c_span.entered();
+                        {
+                            let msg: String = format!("first child event");
+
+                            tracing::event!(Level::WARN, message = msg);
+                            
+                        }
+                        let c_span = c_enter.exit();
+
+                        let c_enter = c_span.enter();
+                        {
+                            let msg: String = format!("second child event");
+
+                            tracing::event!(Level::WARN, message = msg);
+                        }
+                        drop(c_enter);
                     }
+                    drop(p_enter);
+                    // std::thread::sleep(std::time::Duration::from_secs(3600))
                 }
+                drop(gp_enter);
             }
         }))
     }
@@ -63,6 +77,7 @@ async fn main() {
     for t in tasks {
         t.await.unwrap();
     }
+    eprintln!("All tasks completed, shutting down");
 
     controller.shutdown().await;
     handle.await.unwrap();
